089a26cb02928d5fca294af63d682eea1521556e
fermuribar <fermuribar@correo.ugr.es>
2023-03-25 12:18:12 +0100

    borro think

--- Comportamientos_Jugador/jugador.cpp
+++ Comportamientos_Jugador/jugador.cpp
@@ -6,33 +6,7 @@ using namespace std;
 
 Action ComportamientoJugador::think(Sensores sensores){
 
-	Action accion = actFORWARD;	//actIDLE actFORWARD actTURN_SL
-
-	cout << "Posicion: fila " << sensores.posF << " columna " << sensores.posC << " ";
-	switch(sensores.sentido){
-		case 0: cout << "Norte" << endl; break;
-		case 1: cout << "Noreste" << endl; break;
-		case 2: cout << "Este" << endl; break;
-		case 3: cout << "Sureste" << endl; break;
-		case 4: cout << "Sur " << endl; break;
-		case 5: cout << "Suroeste" << endl; break;
-		case 6: cout << "Oeste" << endl; break;
-		case 7: cout << "Noroeste" << endl; break;
-	}
-	cout << "Terreno: ";
-	for (int i=0; i<sensores.terreno.size(); i++)
-		cout << sensores.terreno[i];
-	cout << endl;
-
-	cout << "Superficie: ";
-	for (int i=0; i<sensores.superficie.size(); i++)
-		cout << sensores.superficie[i];
-	cout << endl;
-
-	cout << "Colisión: " << sensores.colision << endl;
-	cout << "Reset: " << sensores.reset << endl;
-	cout << "Vida: " << sensores.vida << endl;
-	cout << endl;
+	
 
 
 	// Determinar el efecto de la ultima accion enviada
