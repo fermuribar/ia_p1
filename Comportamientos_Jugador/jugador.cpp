92692dab255446c3137dea099b9f2f5a2c13bea8
fermuribar <fermuribar@correo.ugr.es>
2023-04-09 17:40:49 +0200

    entrega

--- Comportamientos_Jugador/jugador.cpp
+++ Comportamientos_Jugador/jugador.cpp
@@ -4,52 +4,50 @@ using namespace std;
 
 /*
 ----------------------------------------------CONSTRUCTOR Jugador---------------------------------------------------
-
-
 */
+// Constructor de la clase
 ComportamientoJugador::ComportamientoJugador(unsigned int size) : Comportamiento(size){
-      // Constructor de la clase
-      // Dar el valor inicial a las variables de 
-        //tutorial
-      current_state.fil = current_state.col = mapaResultado.size() - 1; // la posicion sin estar posicionado por defecto es en el centro del mapa visto_sin_bien_situado
-      current_state.brujula = norte;
-	  current_state.chanclas = current_state.bikini = false;
-      last_action = actIDLE;  
-      //giro_derecha = false;
-      bien_situado = false;
-        //fin tuto
 
-      //incicio mapas axiliares
-      vector<int> aux(mapaResultado.size(),0);
-      vector<unsigned char> aux1(mapaResultado.size() * 2, '?');
-      vector<int> aux2(mapaResultado.size() * 2, 0);
-      for( int i = 0;i < mapaResultado.size(); i++)  plan_bien_situado.push_back(aux);
-      for (size_t i = 0; i < mapaResultado.size() * 2; i++){
-        visto_sin_bien_situado.push_back(aux1);
-        plan_sin_bien_situado.push_back(aux2);
-      }
+	// Dar el valor inicial a las variables de estado
+	current_state.fil = current_state.col = mapaResultado.size() - 1; // la posicion sin estar posicionado por defecto es en el centro del mapa visto_sin_bien_situado
+	current_state.brujula = norte;
+	current_state.chanclas = current_state.bikini = false; //al iniciar no se tiene ni zapatillas(chanclas) ni bikini
+	last_action = actIDLE;  //la accion anterior al inico es actIDLE 
+	bien_situado = false;	//por defecto no se aparece bien situado
 
-      //defino el precipicio exteriror
-      for(int i = 0; i < mapaResultado.size(); i++){
-        //borde superiror
-        mapaResultado[0][i] = 'P';
-        mapaResultado[1][i] = 'P';
-        mapaResultado[2][i] = 'P';
-        //borde inferiror
-        mapaResultado[mapaResultado.size()-3][i] = 'P';
-        mapaResultado[mapaResultado.size()-2][i] = 'P';
-        mapaResultado[mapaResultado.size()-1][i] = 'P';
-        //borde izq
-        mapaResultado[i][0] = 'P';
-        mapaResultado[i][1] = 'P';
-        mapaResultado[i][2] = 'P';
-        //borde der
-        mapaResultado[i][mapaResultado.size()-3] = 'P';
-        mapaResultado[i][mapaResultado.size()-2] = 'P';
-        mapaResultado[i][mapaResultado.size()-1] = 'P';
-      }
-      
-    }
+	//incicio mapas axiliares
+		//vectores auxiliares para iniciar las matrices auxiliares
+	vector<int> aux(mapaResultado.size(),0);
+	vector<unsigned char> aux1(mapaResultado.size() * 2, '?');
+	vector<int> aux2(mapaResultado.size() * 2, 0);
+
+	for( int i = 0;i < mapaResultado.size(); i++)  plan_bien_situado.push_back(aux);	//inicializacion de la matriz de puntuacions con el mismo tamaño que el mapa
+	for (size_t i = 0; i < mapaResultado.size() * 2; i++){	//inicializacion de la matriz de puntuaciones y casillas con el doble del tamaño del mapa para almacenar datos cuando no se esta bien posicionado
+		visto_sin_bien_situado.push_back(aux1);
+		plan_sin_bien_situado.push_back(aux2);
+	}
+
+	//defino el precipicio exteriror
+	for(int i = 0; i < mapaResultado.size(); i++){
+		//borde superiror
+		mapaResultado[0][i] = 'P';
+		mapaResultado[1][i] = 'P';
+		mapaResultado[2][i] = 'P';
+		//borde inferiror
+		mapaResultado[mapaResultado.size()-3][i] = 'P';
+		mapaResultado[mapaResultado.size()-2][i] = 'P';
+		mapaResultado[mapaResultado.size()-1][i] = 'P';
+		//borde izq
+		mapaResultado[i][0] = 'P';
+		mapaResultado[i][1] = 'P';
+		mapaResultado[i][2] = 'P';
+		//borde der
+		mapaResultado[i][mapaResultado.size()-3] = 'P';
+		mapaResultado[i][mapaResultado.size()-2] = 'P';
+		mapaResultado[i][mapaResultado.size()-1] = 'P';
+	}
+	
+}
 
 /*
 ----------------------------------------------THINK---------------------------------------------------------------------------------------
@@ -63,53 +61,54 @@ Action ComportamientoJugador::think(Sensores sensores){
 	Action accion = actIDLE;
 
 	// Determinar el efecto de la ultima accion enviada y actualiza las variables estado
-	if(!sensores.colision && !sensores.reset){	//no del tuto
+	if(!sensores.colision && !sensores.reset){ //comprueba si he tenido una colision o he sido reseteado
 		
 		// Actualizacion de posiciones
-		act_posi();
+		act_posi();	//va actualizando mi posicion en funcion de la accion anteriror y la orientacion. Muy importante para los niveles 1 2 3
 
-		//encuentra posicion original
-		if(sensores.terreno[0]=='G' or sensores.posF != -1){
+		//encuentra posicion original. Tambien muy importante para los niveles 1 2 3
+		if(sensores.terreno[0]=='G' or sensores.posF != -1){ //comprueba que estoy en una casilla G de posicionamiento o que directamente mi sensores funcionan. En el nivel 0 siempre se cumple este condicional
 			//actualiza el mapa resultado con lo que vio el agente antes de encontrar casilla G
-			act_visto(sensores);
-			//...
+			//pasa todos los datos de las matrices donde no estaba bien situado a las que si lo estoy
+			act_visto(sensores); 
+			//actualiza los datos de posicionamiento orientacion y modifica la variable bien_situado que nos indica en que mapa|matrices auxiliares podemos trabajar
 			current_state.fil = sensores.posF;
 			current_state.col = sensores.posC;
 			current_state.brujula = sensores.sentido;
 			bien_situado = true;
 		}
 
-		//encuentra chanclas
+		//encuentra bikini 
 		if(!current_state.bikini and sensores.terreno[0] == 'K'){
-			current_state.bikini = true;
-			recalcula_mapas();
+			current_state.bikini = true; //indica que tenemos bikini
+			recalcula_mapas(); //revaloriza las puntuaciones de las casillas considerando la optencion del objeto
 		}
-		//encuentra bikini
+		//encuentra zapatillas
 		if(!current_state.chanclas and sensores.terreno[0] == 'D'){
-			current_state.chanclas = true;
-			recalcula_mapas();
+			current_state.chanclas = true; //indica que tenemos zapatillas
+			recalcula_mapas(); //revaloriza las puntuaciones de las casillas considerando la optencion del objeto
 		}
 
 		// Actualizacion de mapaResultado
-		if(bien_situado){
+		if(bien_situado){	//dependiendo de si conozco mi posicion o no actualiza la matriz con lo visto y doy puntuaciones por defecto
 			act_mapas(sensores, true);
 		}else{
 			act_mapas(sensores, false);
 		}
 
 		// Marca el camino por donde pasa
-		if(last_action == actFORWARD){
-			marca_camino();
+		if(last_action == actFORWARD){ //si la anteriror accion di un paso hacia delante 
+			marca_camino();	//incremento las puntuaciones de la casilla donde me encuentro y la de sus lados 
 		}
 
-	}else if(sensores.reset){
-		bien_situado = false;
-		current_state.fil = current_state.col = mapaResultado.size() - 1; 
-		current_state.brujula = norte;
-		current_state.chanclas = current_state.bikini = false;
-		last_action = actIDLE;
-		recalcula_mapas();
-		borra_visto();
+	}else if(sensores.reset){ //en caso de morir
+		bien_situado = false; //pierdo la orientacion
+		current_state.fil = current_state.col = mapaResultado.size() - 1; //reposiciono en el centro de los mapas auxiliares
+		current_state.brujula = norte; //orientacion norte
+		current_state.chanclas = current_state.bikini = false; //pierdo objetos
+		last_action = actIDLE; //indico que la anterior accion fue quedarme parado para no modificar puntuaciones ni valorar posiciones
+		recalcula_mapas(); //revaloriza las puntuaciones de las casillas considerando la perdida de los objeto
+		borra_visto(); //los mapas auxiliares de cuando no estaba bien posicionado deben de ser borrados. 
 	}
 
 	// Determinar la siguiente accion a realizar:
@@ -129,15 +128,6 @@ int ComportamientoJugador::interact(Action accion, int valor){
 ----------------------------------------------ACTUALIZA DATOS---------------------------------------------------------------------------------------
 -->analisis y tratamiento de accion anterior y sensores
 */
-
-//Borra todo lo visto sin estar bien posicionado (en caso de muerte)
-void ComportamientoJugador::borra_visto(){
-	for(int i = 0; i < visto_sin_bien_situado.size(); i++)
-		for(int j = 0; j< visto_sin_bien_situado.size(); j++){
-			visto_sin_bien_situado[i][j] = '?';
-			plan_sin_bien_situado[i][j] = 0;
-		}
-}
 
 //Actualiza la posicion en funcion de la orientacion y accion anterior
 void ComportamientoJugador::act_posi(){
@@ -183,22 +173,22 @@ void ComportamientoJugador::act_posi(){
 	current_state.brujula = static_cast<Orientacion>(orientacion_tem);
 }
 
-//Define la puntuacion de cada casilla solo basandose en el tipo de casilla
+//Define la puntuacion de cada casilla solo basandose en el tipo de casilla | las puntuaciones se han obtenido para mejorar la puntuacion en la tabla de clasificaciones
 int ComportamientoJugador::valor_casilla(unsigned char c){
 	int valor;
 	switch (c){
 		case 'G': case 'K': case 'D': case 'X': valor = 1; break;
 		case 'S': valor = 1; break;
 		case 'T': valor = 2; break;
-		case 'B': valor = (current_state.chanclas /*and false*/) ? 4 : 4; break;
-		case 'A': valor = (current_state.bikini /*and false*/) ? 7 : 9; break;
+		case 'B': valor = (current_state.chanclas ) ? 4 : 4; break;
+		case 'A': valor = (current_state.bikini ) ? 7 : 9; break;
 		case 'M': case 'P': valor = 10000; break;
 		default: valor = 0; break;
 	}
 	return valor;
 }
 
-//Plasma lo visto antes de posicionarse correctamente
+//Plasma lo visto antes de posicionarse correctamente| actualiza las matrices donde si se estan bien posicionado
 void ComportamientoJugador::act_visto(Sensores sensores){			
 	int diff_fil = current_state.fil - sensores.posF;
 	int diff_col = current_state.col - sensores.posC;
@@ -218,7 +208,7 @@ void ComportamientoJugador::act_visto(Sensores sensores){
 	}
 }
 
-//Gestion de la vision tanto bien posicionado como no
+//Gestion de la vision y de la puntuacion por defecto de las casillas. Tanto bien posicionado como no
 void ComportamientoJugador::act_mapas(Sensores sensores, bool situado){
 	int fil = current_state.fil , col = current_state.col;
     int f = fil, c = col;
@@ -305,6 +295,7 @@ void ComportamientoJugador::act_mapas(Sensores sensores, bool situado){
 	}
 }
 
+//incrementa en una unidad las puntuaciones donde se encuentra el agente y la de su izquierda y derecha. No incrementa las puertas (definiendo una puerta como la casilla entre dos muros o dos precipicios)
 void ComportamientoJugador::marca_camino(){
 	int f = current_state.fil, c = current_state.col;
 	unsigned char l1 , l2;
@@ -361,14 +352,15 @@ void ComportamientoJugador::marca_camino(){
 		l2=(bien_situado) ? mapaResultado[f+1][c] : visto_sin_bien_situado[f+1][c];
 		break;
 	}
-	if((l1 != 'M' or l2 != 'M') and (l1 != 'P' or l2 != 'P'))
+	if((l1 != 'M' or l2 != 'M') and (l1 != 'P' or l2 != 'P')) //para comprobar que la casilla en la que me encuentro no esta entre muros ni entre precipicios
 		(bien_situado) ? plan_bien_situado[f][c]++ : plan_sin_bien_situado[f][c]++;
 
 }
 
+//revaloriza el agua o bosque en funcion de tener o no zapatillas| realmente la funcion que comprueva la tenencia del objeto es valor_casilla
 void ComportamientoJugador::recalcula_mapas(){
 	if(!bien_situado){
-		for(int i = 0; i < visto_sin_bien_situado.size(); i++){
+		for(int i = 0; i < visto_sin_bien_situado.size(); i++){ 
 			for(int j = 0; j < visto_sin_bien_situado.size(); j++){
 				if((visto_sin_bien_situado[i][j] == 'B' or visto_sin_bien_situado[i][j] == 'A'))
 					plan_sin_bien_situado[i][j] = valor_casilla(visto_sin_bien_situado[i][j]);
@@ -383,7 +375,7 @@ void ComportamientoJugador::recalcula_mapas(){
 	}
 }
 
-
+//funcion auxiliar que nos indica si existe una casilla del tipo indicado en nuestra vision. nos devolvera la posicion de esta o -1 en caso de no existir
 int ComportamientoJugador::busca_casilla_vision(Sensores sensores, unsigned char c){
 	int pos = -1;
 	int tam = sensores.terreno.size();
@@ -396,15 +388,24 @@ int ComportamientoJugador::busca_casilla_vision(Sensores sensores, unsigned char
 	return pos;
 }
 
+//Borra todo lo visto sin estar bien posicionado (en caso de muerte)
+void ComportamientoJugador::borra_visto(){
+	for(int i = 0; i < visto_sin_bien_situado.size(); i++)
+		for(int j = 0; j< visto_sin_bien_situado.size(); j++){
+			visto_sin_bien_situado[i][j] = '?';
+			plan_sin_bien_situado[i][j] = 0;
+		}
+}
+
 
 /*
 ----------------------------------------------COMPORTAMIENTO---------------------------------------------------------------------------------------
 --->Decidir accion
 */
-
+//funcion que calcula que accion tomar en funcion de las puntuaciones de las casillas proximas
 Action ComportamientoJugador::suma_puntuaciones(){
-	int recto = 0, der_s = 0, izq_s = 0, der_l = 0, izq_l = 0;
-	int n = 0,ne = 0,e = 0,se = 0,s = 0,so = 0,o = 0, no = 0;
+	int recto = 0, der_s = 0, izq_s = 0, der_l = 0, izq_l = 0; //puntuaciones de las casillas delante derecha izquier ..
+	int n = 0,ne = 0,e = 0,se = 0,s = 0,so = 0,o = 0, no = 0; //las puntuaciones de todas mis casillas ayacentes
 	int f = current_state.fil, c = current_state.col;
 	Action accion;
 	n = (bien_situado) ? plan_bien_situado[f - 1][c]: plan_sin_bien_situado[f - 1][c];
@@ -416,7 +417,7 @@ Action ComportamientoJugador::suma_puntuaciones(){
 	o = (bien_situado) ? plan_bien_situado[f][c - 1]: plan_sin_bien_situado[f][c - 1];
 	no = (bien_situado) ? plan_bien_situado[f - 1][c - 1]: plan_sin_bien_situado[f - 1][c - 1];
 	
-	switch (current_state.brujula){
+	switch (current_state.brujula){ //calculo de las casillas delante derecha izquierda ..
 		case norte: recto = n; der_s = ne; izq_s = no; der_l = se; izq_l = so; break;
 		case noreste: recto = ne; der_s = e; izq_s = n; der_l = s; izq_l = o; break;
 		case este: recto = e; der_s = se; izq_s = ne; der_l = so; izq_l = no; break;
@@ -427,7 +428,7 @@ Action ComportamientoJugador::suma_puntuaciones(){
 		case noroeste: recto = no; der_s = n; izq_s = o; der_l = e; izq_l = s; break;
 	}
 
-	if(recto <= der_s and recto <= izq_s and recto <= der_l and recto <= izq_l){
+	if(recto <= der_s and recto <= izq_s and recto <= der_l and recto <= izq_l){ //toma de decision en funcion de las puntuaciones
 		accion = actFORWARD;
 	}else if(der_s <= izq_s and der_s <= der_l and der_s <= izq_l){
 		accion = actTURN_SR;
@@ -442,7 +443,7 @@ Action ComportamientoJugador::suma_puntuaciones(){
 	return accion;
 }
 
-//Decide la accion a tomar
+//Decide la accion a tomar 
 Action ComportamientoJugador::decide_accion(Sensores sensores){
 	Action accion;
 	int pos_X = busca_casilla_vision(sensores,'X');	//encuentra cargador
@@ -451,10 +452,10 @@ Action ComportamientoJugador::decide_accion(Sensores sensores){
 	int pos_P = busca_casilla_vision(sensores,'P');	//encuentra precipicio
 	int pos_K = busca_casilla_vision(sensores,'K'); //encuentra bikini
 	int pos_D = busca_casilla_vision(sensores,'D'); //encuentra chanclas
-	if(sensores.terreno[0] != 'X'  and sensores.superficie[2]=='_'){
+	if(sensores.terreno[0] != 'X'  and sensores.superficie[2]=='_'){ //comprueba que no tengo entidad delante y no estoy en una casilla de carga
 		
-		if(!bien_situado and pos_G != -1 and pos_M == -1 and pos_P == -1){ 
-
+		if(!bien_situado and pos_G != -1 and pos_M == -1 and pos_P == -1){ //comprueba si existe casilla de posicionamiento y si me hace falta
+			//llegada ha la casilla
 			if(pos_G == 1 or pos_G == 4 or pos_G == 9){
 				accion = actTURN_SL;
 			}else if(pos_G == 3 or pos_G == 8 or pos_G == 15){
@@ -463,8 +464,8 @@ Action ComportamientoJugador::decide_accion(Sensores sensores){
 				accion = actFORWARD;
 			}
 
-		}else if(sensores.bateria < 2500 and pos_X != -1 and pos_M == -1 and pos_P == -1){ 
-
+		}else if(sensores.bateria < 2500 and pos_X != -1 and pos_M == -1 and pos_P == -1){ //comprueba si existe casilla de carga y si me hace falta
+			//llegada ha la casilla
 			if(pos_X == 1 or pos_X == 4 or pos_X == 9){
 				accion = actTURN_SL;
 			}else if(pos_X == 3 or pos_X == 8 or pos_X == 15){
@@ -473,8 +474,8 @@ Action ComportamientoJugador::decide_accion(Sensores sensores){
 				accion = actFORWARD;
 			}
 		
-		}else if(!current_state.chanclas and pos_D != -1 and pos_M == -1 and pos_P == -1){ 
-
+		}else if(!current_state.chanclas and pos_D != -1 and pos_M == -1 and pos_P == -1){ //comprueba si existe casilla de zapatilla y si me hace falta
+			//llegada ha la casilla
 			if(pos_D == 1 or pos_D == 4 or pos_D == 9){
 				accion = actTURN_SL;
 			}else if(pos_D == 3 or pos_D == 8 or pos_D == 15){
@@ -483,8 +484,8 @@ Action ComportamientoJugador::decide_accion(Sensores sensores){
 				accion = actFORWARD;
 			}
 		
-		}else if(!current_state.bikini and pos_K != -1 and pos_M == -1 and pos_P == -1){ 
-
+		}else if(!current_state.bikini and pos_K != -1 and pos_M == -1 and pos_P == -1){ //comprueba si existe casilla de bikini y si me hace falta
+			//llegada ha la casilla
 			if(pos_K == 1 or pos_K == 4 or pos_K == 9){
 				accion = actTURN_SL;
 			}else if(pos_K == 3 or pos_K == 8 or pos_K == 15){
@@ -493,17 +494,17 @@ Action ComportamientoJugador::decide_accion(Sensores sensores){
 				accion = actFORWARD;
 			}
 
-		}else 
+		}else //en caso de no tener estas casillas en mi vision o no hacer falta seguir algoritmo de puntuaciones
 			accion = suma_puntuaciones();
 		
 	}else{
 
-		if(sensores.superficie[2]!='_')
-			accion = (static_cast<double>(rand()) / RAND_MAX == 0) ? actTURN_SR : actTURN_SL;
-		else if((sensores.terreno[0] == 'X' and sensores.bateria < 2500))
-			accion = actIDLE;
-		else 
-			accion = suma_puntuaciones();
+		if(sensores.superficie[2]!='_') //en caso de encontrarme delante una entidad
+			accion = (static_cast<double>(rand()) / RAND_MAX == 0) ? actTURN_SR : actTURN_SL; //gira a derecha o izquierda aleatoriamente
+		else if((sensores.terreno[0] == 'X' and sensores.bateria < 2500)) //en caso de estar en la casilla de carga y necesitar bateria 
+			accion = actIDLE;	//quedarse recargando
+		else //en caso contrario a lo anteriror seguir algoritmo de puntuaciones
+			accion = suma_puntuaciones(); 
 	}
 	return accion;
 }
--- Comportamientos_Jugador/jugador.hpp
+++ Comportamientos_Jugador/jugador.hpp
@@ -25,31 +25,51 @@ class ComportamientoJugador : public Comportamiento{
   private:
 
   // Declarar aquí los metodos
-  void borra_visto();
+  
+  //----------------------------------------------ACTUALIZA DATOS---------------------------------------------------------------------------------------
 
+    //Actualiza la posicion en funcion de la orientacion y accion anterior
   void act_posi();
+
+    //Define la puntuacion de cada casilla solo basandose en el tipo de casilla | Las puntuaciones se han obtenido para mejorar la puntuacion en la tabla de clasificaciones
+      //valora de forma distinta en funcion de tener objetos o no
   int valor_casilla(unsigned char c);
+
+    //Plasma lo visto antes de posicionarse correctamente| actualiza las matrices donde si se estan bien posicionado
   void act_visto(Sensores sensores);
+
+    //Gestion de la vision y de la puntuacion por defecto de las casillas. Tanto bien posicionado como no
   void act_mapas(Sensores sensores, bool situado);
 
+    //incrementa en una unidad las puntuaciones donde se encuentra el agente y la de su izquierda y derecha. No incrementa las puertas (definiendo una puerta como la casilla entre dos muros o dos precipicios)
   void marca_camino();
 
+    //revaloriza el agua o bosque en funcion de tener o no zapatillas| realmente la funcion que comprueva la tenencia del objeto es valor_casilla
   void recalcula_mapas();
-
+    
+    //funcion auxiliar que nos indica si existe una casilla del tipo indicado en nuestra vision. nos devolvera la posicion de esta o -1 en caso de no existir
   int busca_casilla_vision(Sensores sensores, unsigned char c);
 
+    //Borra todo lo visto sin estar bien posicionado (en caso de muerte)
+  void borra_visto();
+
+  //----------------------------------------------COMPORTAMIENTO---------------------------------------------------------------------------------------
+
+    //funcion que calcula que accion tomar en funcion de las puntuaciones de las casillas proximas
   Action suma_puntuaciones();
   Action decide_accion(Sensores sensores);
   
-  // Declarar aquí las variables de estado
-  state current_state;
-  Action last_action;
-
-  bool bien_situado;
   
-  vector< vector<int> > plan_bien_situado;
-  vector< vector<unsigned char> > visto_sin_bien_situado;
-  vector< vector<int> > plan_sin_bien_situado;
+  
+  // Declarar aquí las variables de estado
+  state current_state; //estructura con las posicions, orientacion.. 
+  Action last_action; //ultima accion realizada
+
+  bool bien_situado; //variable para saber en que matriz auxiliar trabajar
+  
+  vector< vector<int> > plan_bien_situado; //matriz de puntuaciones del mismo tamaño que el mapa para indicar "las veces que he pasado por cada casilla"
+  vector< vector<unsigned char> > visto_sin_bien_situado; //matriz para almacenar las casillas vistas mientra el agente no sabia su posicion
+  vector< vector<int> > plan_sin_bien_situado; //matriz para almacenar las puntuaciones mientra el agente no sabia su posicion
 };
 
 #endif
